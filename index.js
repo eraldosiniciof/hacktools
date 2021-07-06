151117ff1be0dddf3f3995ea7dcabe7376cb6747
eraldosiniciof <eraldosinicio@gmail.com>
2021-07-06 11:48:06 -0300

    feat: create server with express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 9901
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/public/index.html`))
+})
+
+app.listen(port, () => {
+  console.log(`Servidor na porta ${port}`)
+})
