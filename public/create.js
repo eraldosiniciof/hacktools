d87dd675051603ef79c5c3badc13565f260c866e
eraldosiniciof <eraldosinicio@gmail.com>
2021-07-06 16:09:11 -0300

    feat: frontend - remove items in list

--- public/create.js
+++ public/create.js
@@ -1,15 +1,35 @@
-document.querySelector('#register').addEventListener('click', () => {
-  const ul = document.querySelector('#list ul')
-  const span = document.createElement('span')
-  const text = document.querySelector('#question')
-  const pElement = document.createElement('p')
-  const liElement = document.createElement('li')
-  const answer = document.createElement('input')
+const ul = document.querySelector('#list ul')
 
+document.querySelector('#register').addEventListener('click', () => {
+  const text = document.querySelector('#question')
+
+  if (text.value === '') {
+    alert('Por favor, escreva uma pergunta.')
+    return
+  }
+
+  const span = document.createElement('span')
+  const liElement = document.createElement('li')
+  const btnRemove = document.createElement('button')
+
+  btnRemove.classList.add('btnRemove')
+  btnRemove.textContent = 'Remover'
   span.textContent = text.value
-  pElement.appendChild(span)
-  answer.classList.add('questions')
-  pElement.appendChild(answer)
-  liElement.appendChild(pElement)
+  liElement.appendChild(span)
+  liElement.appendChild(btnRemove)
   ul.appendChild(liElement)
+
+  text.value = ''
+  text.focus()
+})
+
+document.querySelector('#list').addEventListener('click', (event) => {
+  const target = event.target
+  const child = target.parentNode
+  const description = child.querySelector('span').textContent
+  if (target.textContent === 'Remover') {
+    if (confirm(`Você deseja excluir "${description}" ?`)) {
+      ul.removeChild(child)
+    }
+  }
 })
